6e1fc036a4d547dbc87700eef90ef50443bceba0
JudeTulel <judeyego202@gmail.com>
2025-03-01 00:19:51 +0300

    redder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,17 @@
-"use client"
-import React from 'react'
-import Header from '@/components/header&Footer/Header'
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'; // Correct import
-import Home from '../pages/Home'
-const page = () => {
-  return (
-    <Router> {/* Wrap the entire app with Router */}
-    <div className="bg-[url('https://i.imgur.com/iHdHgmG.png')] bg-cover bg-center p-4 shadow-lg h-screen flex flex-col" >
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="about" element={<About />} /> */}
-      </Routes>
- 
-      </div>
-      
-  </Router>
-  )
-}
+"use client";
+import React from 'react';
+import Header from '@/components/header&Footer/Header';
+import Home from '../pages/Home';
+import Footer from '@/components/header&Footer/Footer';
 
-export default page
+const Page = () => {
+  return (
+    <div className="bg-[url('https://i.imgur.com/iHdHgmG.png')] bg-cover bg-center p-4 shadow-lg h-screen flex flex-col">
+      <Header />
+         <Home />
+      <Footer/>
+    </div>
+  );
+};
+
+export default Page;
